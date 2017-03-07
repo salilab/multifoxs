21684e94f9abaec78dca1bcd3c54120b05a97cf2
Ben Webb <ben@salilab.org>
2017-03-07 21:36:59 +0000

    No need to make job directories group-executable.

--- python/multifoxs/__init__.py
+++ python/multifoxs/__init__.py
@@ -1,12 +1,10 @@
 import saliweb.backend
-import os, sys, stat
 
 class Job(saliweb.backend.Job):
     runnercls = saliweb.backend.SGERunner
 
     def run(self):
         # TODO
-        os.chmod(".", 0775)
         par = open('input.txt', 'r')
         input_line = par.readline().strip()
 
@@ -20,9 +18,6 @@ perl /netapp/sali/dina/MultiFoXSServer/runMultiFoXS.pl %s >& multifoxs.log
         r.set_sge_options('-l arch=linux-x64,h_rt=300:00:00,mem_free=4G -p 0')
         #r.set_sge_options('-l arch=linux-x64,mem_free=4G -p 0')
         return r
-
-    def complete(self):
-        os.chmod(".", 0775)
 
 class Config(saliweb.backend.Config):
     def populate(self, config):
