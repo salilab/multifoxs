423260e9041f6c11b3715b2986d7e56638659926
Ben Webb <ben@salilab.org>
2021-01-12 14:35:12 -0800

    Use context managers to make tmpdirs

--- test/backend/test_post.py
+++ test/backend/test_post.py
@@ -8,44 +8,40 @@ class Tests(saliweb.test.TestCase):
     def test_postprocess_ok(self):
         """Test postprocess with everything OK"""
         j = self.make_test_job(multifoxs.Job, 'RUNNING')
-        d = saliweb.test.RunInDir(j.directory)
-        for fname in ('multifoxs.log', 'chis.png', 'hist.png'):
-            with open(fname, 'w') as fh:
-                fh.write("everything ok\n")
-        j.postprocess()
-        del d
+        with saliweb.test.working_directory(j.directory):
+            for fname in ('multifoxs.log', 'chis.png', 'hist.png'):
+                with open(fname, 'w') as fh:
+                    fh.write("everything ok\n")
+            j.postprocess()
 
     def test_postprocess_missing_outputs(self):
         """Test postprocess with missing outputs"""
         j = self.make_test_job(multifoxs.Job, 'RUNNING')
-        d = saliweb.test.RunInDir(j.directory)
-        for fname in ('multifoxs.log', 'chis.png'):
-            with open(fname, 'w') as fh:
-                fh.write("everything ok\n")
-        self.assertRaises(multifoxs.MissingOutputsError, j.postprocess)
-        del d
+        with saliweb.test.working_directory(j.directory):
+            for fname in ('multifoxs.log', 'chis.png'):
+                with open(fname, 'w') as fh:
+                    fh.write("everything ok\n")
+            self.assertRaises(multifoxs.MissingOutputsError, j.postprocess)
 
     def test_postprocess_bad_log(self):
         """Test postprocess with bad log file"""
         j = self.make_test_job(multifoxs.Job, 'RUNNING')
-        d = saliweb.test.RunInDir(j.directory)
-        for fname in ('chis.png', 'hist.png'):
-            with open(fname, 'w') as fh:
-                fh.write("everything ok\n")
-        with open('multifoxs.log', 'w') as fh:
-            fh.write("ERROR: Invalid input profile\n")
-        self.assertTrue(j.check_log_file())
-        j.postprocess()  # Should run without failure
-        del d
+        with saliweb.test.working_directory(j.directory):
+            for fname in ('chis.png', 'hist.png'):
+                with open(fname, 'w') as fh:
+                    fh.write("everything ok\n")
+            with open('multifoxs.log', 'w') as fh:
+                fh.write("ERROR: Invalid input profile\n")
+            self.assertTrue(j.check_log_file())
+            j.postprocess()  # Should run without failure
 
     def test_postprocess_log_error(self):
         """Test postprocess with log internal error"""
         j = self.make_test_job(multifoxs.Job, 'RUNNING')
-        d = saliweb.test.RunInDir(j.directory)
-        with open('multifoxs.log', 'w') as fh:
-            fh.write("gnuplot: command not found\n")
-        self.assertRaises(multifoxs.LogError, j.check_log_file)
-        del d
+        with saliweb.test.working_directory(j.directory):
+            with open('multifoxs.log', 'w') as fh:
+                fh.write("gnuplot: command not found\n")
+            self.assertRaises(multifoxs.LogError, j.check_log_file)
 
 
 if __name__ == '__main__':
--- test/frontend/test_submit.py
+++ test/frontend/test_submit.py
@@ -13,81 +13,84 @@ class Tests(saliweb.test.TestCase):
 
     def test_submit_page_pdb_no_atoms(self):
         """Test submit page with PDB containing no ATOM records"""
-        incoming = saliweb.test.TempDir()
-        multifoxs.app.config['DIRECTORIES_INCOMING'] = incoming.tmpdir
-        c = multifoxs.app.test_client()
-        t = saliweb.test.TempDir()
-        pdbfile = os.path.join(t.tmpdir, 'test.pdb')
-        with open(pdbfile, 'w') as fh:
-            fh.write("garbage\n")
+        with saliweb.test.temporary_directory() as tmpdir:
+            incoming = os.path.join(tmpdir, 'incoming')
+            os.mkdir(incoming)
+            multifoxs.app.config['DIRECTORIES_INCOMING'] = incoming
+            c = multifoxs.app.test_client()
+            pdbfile = os.path.join(tmpdir, 'test.pdb')
+            with open(pdbfile, 'w') as fh:
+                fh.write("garbage\n")
 
-        rv = c.post(
-            '/job', data={'pdbfile': open(pdbfile, 'rb'),
-                          'modelsnumber': "5", "units": "unknown"})
-        self.assertEqual(rv.status_code, 400)
-        self.assertIn(b'PDB file contains no ATOM records', rv.data)
+            rv = c.post(
+                '/job', data={'pdbfile': open(pdbfile, 'rb'),
+                              'modelsnumber': "5", "units": "unknown"})
+            self.assertEqual(rv.status_code, 400)
+            self.assertIn(b'PDB file contains no ATOM records', rv.data)
 
     def test_submit_page(self):
         """Test submit page"""
-        incoming = saliweb.test.TempDir()
-        multifoxs.app.config['DIRECTORIES_INCOMING'] = incoming.tmpdir
-        c = multifoxs.app.test_client()
+        with saliweb.test.temporary_directory() as tmpdir:
+            incoming = os.path.join(tmpdir, 'incoming')
+            os.mkdir(incoming)
+            multifoxs.app.config['DIRECTORIES_INCOMING'] = incoming
+            c = multifoxs.app.test_client()
 
-        t = saliweb.test.TempDir()
-        pdbf = os.path.join(t.tmpdir, 'test.pdb')
-        with open(pdbf, 'w') as fh:
-            fh.write(
-                "REMARK\n"
-                "ATOM      2  CA  ALA     1      26.711  14.576   5.091\n")
-        saxsf = os.path.join(t.tmpdir, 'test.profile')
-        with open(saxsf, 'w') as fh:
-            fh.write("0.00000    9656627.00000000 2027.89172363\n")
-        linkf = os.path.join(t.tmpdir, 'test.linkers')
-        with open(linkf, 'w') as fh:
-            fh.write("189 A\n")
+            pdbf = os.path.join(tmpdir, 'test.pdb')
+            with open(pdbf, 'w') as fh:
+                fh.write(
+                    "REMARK\n"
+                    "ATOM      2  CA  ALA     1      26.711  14.576   5.091\n")
+            saxsf = os.path.join(tmpdir, 'test.profile')
+            with open(saxsf, 'w') as fh:
+                fh.write("0.00000    9656627.00000000 2027.89172363\n")
+            linkf = os.path.join(tmpdir, 'test.linkers')
+            with open(linkf, 'w') as fh:
+                fh.write("189 A\n")
 
-        data = {}
-        # Invalid models number
-        rv = c.post('/job', data=data)
-        self.assertEqual(rv.status_code, 400)
-        self.assertIn(b'Invalid value for number of models', rv.data)
-        data['modelsnumber'] = '100'
+            data = {}
+            # Invalid models number
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 400)
+            self.assertIn(b'Invalid value for number of models', rv.data)
+            data['modelsnumber'] = '100'
 
-        # Invalid units
-        rv = c.post('/job', data=data)
-        self.assertEqual(rv.status_code, 400)
-        self.assertIn(b'Invalid units', rv.data)
-        data['units'] = 'unknown'
+            # Invalid units
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 400)
+            self.assertIn(b'Invalid units', rv.data)
+            data['units'] = 'unknown'
 
-        # Successful submission (no email)
-        data = {'modelsnumber': '100', 'units': 'unknown',
-                'pdbfile': open(pdbf, 'rb'), 'saxsfile': open(saxsf, 'rb'),
-                'hingefile': open(linkf, 'rb'), 'jobname': 'foobar'}
-        rv = c.post('/job', data=data)
-        self.assertEqual(rv.status_code, 200)
-        r = re.compile(b'Your job <b>foobar</b> has been submitted.*'
-                       b'Results will be found at',
-                       re.MULTILINE | re.DOTALL)
-        self.assertRegex(rv.data, r)
+            # Successful submission (no email)
+            data = {'modelsnumber': '100', 'units': 'unknown',
+                    'pdbfile': open(pdbf, 'rb'), 'saxsfile': open(saxsf, 'rb'),
+                    'hingefile': open(linkf, 'rb'), 'jobname': 'foobar'}
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 200)
+            r = re.compile(b'Your job <b>foobar</b> has been submitted.*'
+                           b'Results will be found at',
+                           re.MULTILINE | re.DOTALL)
+            self.assertRegex(rv.data, r)
 
-        # Make sure data.txt is generated
-        with open(os.path.join(incoming.tmpdir, 'foobar', 'data.txt')) as fh:
-            contents = fh.read()
-        self.assertEqual(
-            contents,
-            "input.pdb test.linkers test.profile None foobar None 100\n")
+            # Make sure data.txt is generated
+            with open(os.path.join(incoming, 'foobar', 'data.txt')) as fh:
+                contents = fh.read()
+            self.assertEqual(
+                contents,
+                "input.pdb test.linkers test.profile None foobar None 100\n")
 
-        # Successful submission (with email)
-        data = {'modelsnumber': '100', 'units': 'unknown',
-                'pdbfile': open(pdbf, 'rb'), 'saxsfile': open(saxsf, 'rb'),
-                'hingefile': open(linkf, 'rb'), 'email': 'test@example.com'}
-        rv = c.post('/job', data=data)
-        self.assertEqual(rv.status_code, 200)
-        r = re.compile(b'Your job <b>job\\S+</b> has been submitted.*'
-                       b'Results will be found at.*'
-                       b'You will be notified at test@example.com when',
-                       re.MULTILINE | re.DOTALL)
-        self.assertRegex(rv.data, r)
+            # Successful submission (with email)
+            data = {'modelsnumber': '100', 'units': 'unknown',
+                    'pdbfile': open(pdbf, 'rb'), 'saxsfile': open(saxsf, 'rb'),
+                    'hingefile': open(linkf, 'rb'),
+                    'email': 'test@example.com'}
+            rv = c.post('/job', data=data)
+            self.assertEqual(rv.status_code, 200)
+            r = re.compile(b'Your job <b>job\\S+</b> has been submitted.*'
+                           b'Results will be found at.*'
+                           b'You will be notified at test@example.com when',
+                           re.MULTILINE | re.DOTALL)
+            self.assertRegex(rv.data, r)
 
 
 if __name__ == '__main__':
